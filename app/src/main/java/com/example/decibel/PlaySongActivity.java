f4df1ef6c352cb1666e21eca3945472f68a2897f
Nitish <tnitish654@gmail.com>
2021-07-04 19:23:46 +0800

    Decibel (Adaptive background for PlaySongActivity, Spinning disk for PlaySongActivity)

--- app/src/main/java/com/example/decibel/PlaySongActivity.java
+++ app/src/main/java/com/example/decibel/PlaySongActivity.java
@@ -2,41 +2,50 @@ package com.example.decibel;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.palette.graphics.Palette;
 
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageButton;
-import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.decibel.R;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.io.IOException;
-import java.time.Instant;
 
 public class PlaySongActivity extends AppCompatActivity {
 
     private String title = "";
     private String artist = "";
-    private String fileLink= "";
+    private String fileLink = "";
     private String coverArt;
     private int currentIndex = -1;
 
     private MediaPlayer player = new MediaPlayer();
     private SongCollection songCollection = new SongCollection();
+    private ConstraintLayout playSongConstraint;
+    RotateAnimation rotateAnimation;
     SeekBar seekBar;
     ImageButton btnPlayPause;
+    ImageView background;
+    Bitmap bitmapImage;
     Handler handler = new Handler();
 
     private Palette.Swatch vibrantSwatch;
@@ -45,16 +54,17 @@ public class PlaySongActivity extends AppCompatActivity {
     private Palette.Swatch mutedSwatch;
     private Palette.Swatch lightMutedSwatch;
     private Palette.Swatch darkMutedSwatch;
+    private Palette.Swatch dominantSwatch;
 
 
-    public void displaySongBasedOnIndex(int selectedIndex){
+    public void displaySongBasedOnIndex(int selectedIndex) {
 
         Song song = songCollection.getCurrentSong(currentIndex);
 
         this.title = song.getTitle();
         this.artist = song.getArtist();
         this.fileLink = song.getFindLink();
-        this.coverArt  = song.getCoverArt();
+        this.coverArt = song.getCoverArt();
 
         TextView txtTitle = findViewById(R.id.txtSongTitle);
         txtTitle.setText(title);
@@ -65,27 +75,8 @@ public class PlaySongActivity extends AppCompatActivity {
         ImageView iCoverArt = findViewById(R.id.imgCoverArt);
         Picasso.get().load(coverArt).into(iCoverArt);
 
-        Bitmap bitmap = ((BitmapDrawable) iCoverArt.getDrawable()).getBitmap();
-
-        Palette.from(bitmap).maximumColorCount(24).generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(@Nullable @org.jetbrains.annotations.Nullable Palette palette) {
-                vibrantSwatch = palette.getVibrantSwatch();
-                lightVibrantSwatch = palette.getLightVibrantSwatch();
-                darkVibrantSwatch = palette.getDarkVibrantSwatch();
-                mutedSwatch = palette.getMutedSwatch();
-                lightMutedSwatch = palette.getLightMutedSwatch();
-                darkMutedSwatch = palette.getDarkMutedSwatch();
-            }
-        });
-
-
-
-
-
-
+        backgroudTint();
     }
-
 
 
     public void playSong(String songUrl) {
@@ -95,11 +86,12 @@ public class PlaySongActivity extends AppCompatActivity {
             player.prepare();
             player.start();
             handler.removeCallbacks(progressBar);
-            handler.postDelayed(progressBar, 1000); //activates runnable
+            handler.postDelayed(progressBar, 10); //activates runnable
             gracefullyStopWhenMusicEnds();
             setTitle(title);
-        }
-        catch (IOException e) {
+            spinCoverArt();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -109,13 +101,102 @@ public class PlaySongActivity extends AppCompatActivity {
             player.pause();
             btnPlayPause.setImageResource(android.R.drawable.ic_media_play);
             handler.removeCallbacks(progressBar);
-        }
-        else {
+
+
+        } else {
             player.start();
             btnPlayPause.setImageResource(android.R.drawable.ic_media_pause);
             handler.postDelayed(progressBar, 0);
+            spinCoverArt();
         }
     }
+
+
+    public void spinCoverArt() {
+            rotateAnimation = new RotateAnimation(0, 360f,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+
+            rotateAnimation.setInterpolator(new LinearInterpolator());
+            rotateAnimation.setDuration(3000);
+            rotateAnimation.setRepeatCount(Animation.INFINITE);
+
+            findViewById(R.id.imgCoverArt).startAnimation(rotateAnimation);
+
+        rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                isSongPlaying();
+            }
+        });
+    }
+
+    public void isSongPlaying(){
+        if(player.isPlaying()){
+            Log.d("spin", "Player is playing");
+        }
+        else{
+            findViewById(R.id.imgCoverArt).clearAnimation();
+        }
+
+    }
+
+    public void backgroudTint() {
+
+        vibrantSwatch = null;
+        lightVibrantSwatch = null;
+        darkVibrantSwatch = null;
+        mutedSwatch = null;
+        lightMutedSwatch = null;
+        darkMutedSwatch = null;
+        dominantSwatch = null;
+
+        Picasso.get().load(this.coverArt).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Log.d("Colour", "bitmap loaded");
+                Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(@Nullable @org.jetbrains.annotations.Nullable Palette palette) {
+                        vibrantSwatch = palette.getVibrantSwatch();
+                        lightVibrantSwatch = palette.getLightVibrantSwatch();
+                        darkVibrantSwatch = palette.getDarkVibrantSwatch();
+                        mutedSwatch = palette.getMutedSwatch();
+                        lightMutedSwatch = palette.getLightMutedSwatch();
+                        darkMutedSwatch = palette.getDarkMutedSwatch();
+                        dominantSwatch = palette.getDominantSwatch();
+                        Log.d("Colour", "the swatch is " + vibrantSwatch);
+
+                        if (dominantSwatch != null){
+                            Log.d("Colour", "the dominant colour is " + dominantSwatch.getRgb());
+                            background.setBackgroundColor(dominantSwatch.getRgb());
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                Log.d("Colour", "bitmap fail");
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                Log.d("Colour", "bitmap prepare");
+            }
+        });
+    }
+
 
     public void playNext(View view) {
         currentIndex = songCollection.getNextSong(currentIndex);
@@ -124,6 +205,7 @@ public class PlaySongActivity extends AppCompatActivity {
         Log.d("temasek", "After playNext, the index is now :" + currentIndex);
         displaySongBasedOnIndex(currentIndex);
         playSong(fileLink);
+        backgroudTint();
     }
 
     public void playPrevious(View view) {
@@ -133,6 +215,7 @@ public class PlaySongActivity extends AppCompatActivity {
         Log.d("temasek", "After playPrevious, the index is now :" + currentIndex);
         displaySongBasedOnIndex(currentIndex);
         playSong(fileLink);
+        backgroudTint();
     }
 
 
@@ -173,7 +256,7 @@ public class PlaySongActivity extends AppCompatActivity {
 
         seekBar = findViewById(R.id.seekBar);
         btnPlayPause = findViewById(R.id.btnPlayPause);
-
+        background = findViewById(R.id.background);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -191,6 +274,7 @@ public class PlaySongActivity extends AppCompatActivity {
                 player.seekTo(seekBar.getProgress());
             }
         });
+
     }
 
 
